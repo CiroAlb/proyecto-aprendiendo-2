faf92f9877eaf5f9629923380a1c916719876f81
CiroAlb <albornozciro@gmail.com>
2023-08-14 20:42:09 -0300

    comit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Search from './nav-bar/Search';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="App flex justify-center items-center min-h-screen">
+      <div className="">
+        <Search />
+      </div>
     </div>
   );
 }
