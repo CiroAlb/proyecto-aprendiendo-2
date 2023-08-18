91d47e0a0cb5191dac1ec2443e755efe3e689041
CiroAlb <albornozciro@gmail.com>
2023-08-18 15:13:10 -0300

    commit

--- proyecto-aprendiendo-2/src/barra-lateral/MainTemperatura.js
+++ proyecto-aprendiendo-2/src/barra-lateral/MainTemperatura.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {MyContext} from '../App';
 
 
@@ -22,21 +22,24 @@ const MainTemperatura = () => {
   selectedCityCoordinates, 
     } = useContext(MyContext);
 
-    const apiKey = '54a2afae00970230246059b4ade50106';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${selectedCityCoordinates.latitud}&lon=${selectedCityCoordinates.longitud}&appid=${apiKey}&units=metric`;
+  const [isMetric, setIsMetric] = useState(true); 
+  const apiKey = '54a2afae00970230246059b4ade50106';
+  const unit = isMetric ? 'metric' : 'imperial'; // Elegir unidad
+  const apiUrl = ` https://api.openweathermap.org/data/2.5/weather?lat=${selectedCityCoordinates.latitude}&lon=${selectedCityCoordinates.longitude}&appid=${apiKey}&units=${unit}`;
+ 
 
     const fetchData = async () => {
         try {
           const response = await fetch(apiUrl);
           const data = await response.json();
-    
+          console.log(data);
           setTemperatura(data.main.temp);
           setSensacionTermica(data.main.feels_like);
           setDiaYHora(new Date(data.dt * 1000));
           setTemperaturaMinima(data.main.temp_min);
           setTemperaturaMaxima(data.main.temp_max);
-          setPorcentajeLluvia(data.porcentajeLluvia);
-          setwWeatherDescription(data.weather[0].description);
+          setPorcentajeLluvia(data.rain);
+          setwWeatherDescription(data.weather[0].main);
         } catch (error) {
           console.error('Error fetching weather data:', error);
         }
@@ -53,16 +56,23 @@ const MainTemperatura = () => {
           clearInterval(interval); // Limpiar el intervalo al desmontar el componente
         };
       }, [apiUrl]);
+
+      const toggleUnits = () => {
+        setIsMetric(!isMetric);
+      };
     
       return (
         <div>
-          <p>Temperatura: {temperatura}°C</p>
-          <p>Sensación Térmica: {sensacionTermica}°C</p>
-          <p>Día y Hora: {diaYHora.toLocaleString()}</p>
-          <p>Temperatura Mínima: {temperaturaMinima}°C</p>
-          <p>Temperatura Máxima: {temperaturaMaxima}°C</p>
-          <p>Porcentaje de Lluvia: {porcentajeLluvia}%</p>
-          <p>Descripción del Clima: {weatherDescription}</p>
+          <p>Temperatura: {temperatura}{isMetric ? '°C' : '°F'}</p>
+          <p>Sensación Térmica: {sensacionTermica}{isMetric ? '°C' : '°F'}</p>
+          <p>{diaYHora.toLocaleString()}</p>
+          <p>Min: {temperaturaMinima}{isMetric ? '°C' : '°F'}</p>
+          <p>Max: {temperaturaMaxima}{isMetric ? '°C' : '°F'}</p>
+          <p>Porcentaje de Lluvia: {porcentajeLluvia}%</p> {/*No sirve*/}
+          <p>Descripción del Clima: {weatherDescription}</p>{/*No sirve*/}
+          <button onClick={toggleUnits}>
+        Toggle Units ({isMetric ? 'Metrico' : 'Imperial'})
+      </button>
         </div>
       );
 
