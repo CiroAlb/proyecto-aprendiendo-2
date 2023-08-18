4c61666be6a4cd68bc56d60907ca9bfa6241bcb5
CiroAlb <albornozciro@gmail.com>
2023-08-18 10:59:42 -0300

    commit

--- proyecto-aprendiendo-2/src/barra-lateral/MainTemperatura.js
+++ proyecto-aprendiendo-2/src/barra-lateral/MainTemperatura.js
@@ -25,25 +25,33 @@ const MainTemperatura = () => {
     const apiKey = '54a2afae00970230246059b4ade50106';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${selectedCityCoordinates.latitud}&lon=${selectedCityCoordinates.longitud}&appid=${apiKey}&units=metric`;
 
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await fetch(apiUrl);
-            const data = await response.json();
+    const fetchData = async () => {
+        try {
+          const response = await fetch(apiUrl);
+          const data = await response.json();
     
-            setTemperatura(data.main.temp);
-            setSensacionTermica(data.main.feels_like);
-            setDiaYHora(new Date(data.dt * 1000));
-            setTemperaturaMinima(data.main.temp_min);
-            setTemperaturaMaxima(data.main.temp_max);
-            setPorcentajeLluvia(data.porcentajeLluvia);
-            setwWeatherDescription(data.weather[0].description);
-          } catch (error) {
-            console.error('Error fetching weather data:', error);
-          }
+          setTemperatura(data.main.temp);
+          setSensacionTermica(data.main.feels_like);
+          setDiaYHora(new Date(data.dt * 1000));
+          setTemperaturaMinima(data.main.temp_min);
+          setTemperaturaMaxima(data.main.temp_max);
+          setPorcentajeLluvia(data.porcentajeLluvia);
+          setwWeatherDescription(data.weather[0].description);
+        } catch (error) {
+          console.error('Error fetching weather data:', error);
+        }
+      };
+    
+      useEffect(() => {
+        fetchData(); // Ejecutar la función fetchData al montar el componente
+    
+        const interval = setInterval(() => {
+          fetchData(); // Ejecutar la función fetchData cada minuto
+        }, 60000); // 60000 milisegundos = 1 minuto
+    
+        return () => {
+          clearInterval(interval); // Limpiar el intervalo al desmontar el componente
         };
-    
-        fetchData();
       }, [apiUrl]);
     
       return (
