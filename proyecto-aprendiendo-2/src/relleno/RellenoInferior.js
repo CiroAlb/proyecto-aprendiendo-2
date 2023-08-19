a414eb11aab5ccc45161878cee1bc5f7626a9dce
CiroAlb <albornozciro@gmail.com>
2023-08-18 22:55:12 -0300

    as

--- proyecto-aprendiendo-2/src/relleno/RellenoInferior.js
+++ proyecto-aprendiendo-2/src/relleno/RellenoInferior.js
@@ -29,19 +29,6 @@ const MainTemperatura = () => {
     return `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
   };
 
-  const getRandomColor = () => {
-    const colors = [
-      'bg-red-300',
-      'bg-blue-300',
-      'bg-green-300',
-      'bg-yellow-300',
-      'bg-purple-300',
-      'bg-pink-300',
-    ];
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    return colors[randomIndex];
-  };
-
   return (
     <div className="h-full w-full bg-red-600 ">
       {weatherData && (
